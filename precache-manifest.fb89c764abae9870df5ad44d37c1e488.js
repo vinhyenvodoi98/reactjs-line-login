14ddb1fac2ebc32d58a45605f40fa4d55d9e93aa
hoang <vinhyenvodoi98@gmail.com>
2020-07-29 22:57:02 +0700

    Updates

---
+++ precache-manifest.fb89c764abae9870df5ad44d37c1e488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e92015828a3233722b2e211c8536fa4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bba31172adf0b9209bb6",
+    "url": "./static/css/main.f042ef8a.chunk.css"
+  },
+  {
+    "revision": "904dcf7aaa79d1db33f6",
+    "url": "./static/js/2.5de5f467.chunk.js"
+  },
+  {
+    "revision": "801455085fc6f57b23eb009849543052",
+    "url": "./static/js/2.5de5f467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba31172adf0b9209bb6",
+    "url": "./static/js/main.46040536.chunk.js"
+  },
+  {
+    "revision": "1fdf0c6211c0dd831ac5",
+    "url": "./static/js/runtime-main.7a806cbc.js"
+  }
+]);
